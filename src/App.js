73a622d5c1d10a7da893332eb5ec4a51a495e695
jeddy2023 <jkenosuh@gmail.com>
2023-09-08 17:25:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import Contact from "./page/Contact";
+import Cars from "./page/Cars";
+import About from "./page/About";
+import Login from "./page/Login"
+import Signup from "./page/Signup"
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/cars" element={<Cars />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
